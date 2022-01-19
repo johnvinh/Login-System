d94585cb0779c800bd29f52db995c8bb0dbd4c97
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-19 16:57:41 -0500

    finished basic login html

--- index.php
+++ index.php
@@ -9,8 +9,16 @@
 </header>
 <main>
     <h2>Login</h2>
-    <form>
-        
+    <form action="login.php" method="post">
+        <div>
+            <label for="username-login">Username</label>
+            <input type="text" id="username-login" name="username">
+        </div>
+        <div>
+            <label for="password-login">Password</label>
+            <input type="password" id="password-login" name="password">
+        </div>
+        <input type="submit" name="submit" value="Login">
     </form>
     <h2>Sign-up</h2>
     <form action="signup.php" method="post">
@@ -26,7 +34,7 @@
             <label for="password-signup-repeat">Repeat Password</label>
             <input type="password" id="password-signup-repeat" name="password-repeat">
         </div>
-        <input type="submit" name="submit" value="Log-in">
+        <input type="submit" name="submit" value="Sign Up">
     </form>
 </main>
 </body>
